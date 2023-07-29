0baec98449220e6cc2b08a339785401e061a2165
meongteam <gaqumeku@gmail.com>
2023-07-29 18:45:56 +0700

    raja web

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors :{
+        'text' : '#fae0ba',
+        'title' : '#BB6B24'
+      }
+    },
+  },
+  plugins: [],
+}
+
